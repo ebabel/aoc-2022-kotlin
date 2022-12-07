c4fb2ed51320f21fde038e0ab5d2c1f92457c628
Erik Babel <erik.babel@gmail.com>
2022-12-07 13:17:48 -0800

    Prep for new days

--- src/main/kotlin/y2015/Day17.kt
+++ src/main/kotlin/y2015/Day17.kt
@@ -1,16 +1,16 @@
-package y2022
+package y2015
 fun main(args: Array<String>) {
-    val testInput = Day8(testInput)
+    val testInput = Day17(testInput)
     println("Part1 test ${testInput.part1().also { check(it == "result") }}")
     println("Part2 test ${testInput.part2().also { check(it == "result") }}")
 
-    val realInput = Day8(input)
+    val realInput = Day17(input)
     println("Part1 real ${realInput.part1().also { check(it == "result") }}")
     println("Part2 real ${realInput.part2().also { check(it == "result") }}")
 
 }
 
-class Day8(val input: String) {
+class Day17(val input: String) {
 
     fun part1(): String {
 
