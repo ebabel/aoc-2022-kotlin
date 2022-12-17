1c6f1fe923243b0f077a32ad61d7d106d4734c5f
Erik Babel <erik.babel@gmail.com>
2022-12-16 20:57:02 -0800

    Add measure to template

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,21 +1,27 @@
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
+@OptIn(ExperimentalTime::class)
 fun main(args: Array<String>) {
+    measureTime {
+        val testInput = Template(testInput)
+        testInput.part1()
+            .also { println("Part1 test $it") }
+            .also { check(it == "result") }
+        testInput.part2()
+            .also { println("Part2 test $it") }
+            .also { check(it == "result") }
 
-    val testInput = Template(testInput)
-    testInput.part1()
-        .also { println("Part1 test $it") }
-        .also { check(it == "result") }
-    testInput.part2()
-        .also { println("Part2 test $it") }
-        .also { check(it == "result") }
-
-    val realInput = Template(input)
-    realInput.part1()
-        .also { println("Part1 real $it") }
-        .also { check(it == "result") }
-    realInput.part2()
-        .also { println("Part2 real $it") }
-        .also { check(it == "result") }
+        val realInput = Template(input)
+        realInput.part1()
+            .also { println("Part1 real $it") }
+            .also { check(it == "result") }
+        realInput.part2()
+            .also { println("Part2 real $it") }
+            .also { check(it == "result") }
+    }.also {
+        println("Took ${it.inWholeSeconds} seconds or ${it.inWholeMilliseconds}ms.")
+    }
 }
 
 class Template(private val input: String) {
@@ -30,7 +36,6 @@ class Template(private val input: String) {
         return "result"
     }
 }
-
 
 
 private val testInput =
